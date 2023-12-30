0d9ec5a3352079e5a15a6571cca9e08499d21564
mdconaway <mdconaway@noreply.github.com>
2023-12-30 11:29:00 -0500

    Update unit extractor to handle full manifest

--- examples/vocoder36/extract_units.py
+++ examples/vocoder36/extract_units.py
@@ -1,11 +1,14 @@
 # Proto script to use new unit extractor from outside library
+import os
 import argparse
 import logging
 
 import torch
 from torch import Tensor
 
+from tqdm import tqdm
 from seamless_communication.models.unit_extractor import UnitExtractor
+from examples.textless_nlp.gslm.speech2unit.clustering.utils import get_audio_files
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,7 +18,12 @@ def main():
     parser = argparse.ArgumentParser(
         description="Convert raw audio to units (and optionally audio) using UnitExtractor."
     )
-    parser.add_argument("audio", type=str, help="Audio WAV file path.")
+    parser.add_argument(
+        "--manifest_path",
+        type=str,
+        default=None,
+        help="Manifest file containing the root dir and file names",
+    )
     parser.add_argument(
         "--kmeans_uri",
         type=str,
@@ -34,6 +42,15 @@ def main():
         help="Layer number of the feature extraction model to pull out features from.",
         default=35,
     )
+    parser.add_argument(
+        "--out_quantized_file_path",
+        required=True,
+        type=str,
+        help="File path of quantized output.",
+    )
+    parser.add_argument(
+        "--extension", type=str, default=".wav", help="Audio file extension"
+    )
 
     args = parser.parse_args()
 
@@ -44,10 +61,20 @@ def main():
         device = torch.device("cpu")
         logger.info("Running unit_extraction on the CPU.")
 
+    root_dir, fnames, _ = get_audio_files(args.manifest_path)
+
     unit_extractor = UnitExtractor(args.model_name, args.kmeans_uri, device=device)
-    units: Tensor = unit_extractor.predict(args.audio, args.out_layer_number - 1)
-    units_list = units.tolist()
-    print(f"{' '.join(str(x) for x in units_list)}")
+
+    os.makedirs(os.path.dirname(args.out_quantized_file_path), exist_ok=True)
+    logger.info(f"Writing quantized predictions to {args.out_quantized_file_path}")
+
+    with open(args.out_quantized_file_path, "w") as fout:
+        for i in tqdm(fnames, total=len(fnames)):
+            units: Tensor = unit_extractor.predict(os.path.join(root_dir, i), args.out_layer_number - 1)
+            units_list = units.tolist()
+            pred_str = ' '.join(str(x) for x in units_list)
+            base_fname = os.path.basename(i).rstrip(args.extension)
+            fout.write(f"{base_fname}|{pred_str}\n")
 
 
 if __name__ == "__main__":
